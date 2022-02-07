9833d546f1ccee9d5e1ab1f06d0a73fd01bbd833
Samuel Martins <francisco.martins@vaivoa.com>
2022-02-07 16:42:58 -0300

    first project config

---
+++ AppCaixas/view/HomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeViewController.swift
+//  AppCaixas
+//
+//  Created by FRANCISCO SAMUEL DA SILVA MARTINS on 07/02/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    static let identifier = "HomeViewController"
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "teste"
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
