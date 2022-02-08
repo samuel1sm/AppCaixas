04b2f58ec8d002b5465025c61055910aa3de1fcd
Samuel Martins <francisco.martins@vaivoa.com>
2022-02-08 11:15:53 -0300

    BoxCell creation

---
+++ AppCaixas/view/components/BoxesViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BoxesViewCell.swift
+//  AppCaixas
+//
+//  Created by FRANCISCO SAMUEL DA SILVA MARTINS on 08/02/22.
+//
+
+import UIKit
+
+class BoxesViewCell: UITableViewCell {
+    static let identifier = "BoxesViewCell"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
