9079389c96f2ea6e5aadd4f31ed203d40cc4adfb
Samuel Martins <francisco.martins@modal.com.br>
2022-02-25 15:13:00 -0300

    adding images boxes

--- AppCaixas/utils/Constants.swift
+++ AppCaixas/utils/Constants.swift
@@ -7,6 +7,12 @@
 
 import Foundation
 
-struct Constans {
-
+enum BoxImages: String {
+    case lightBlueBox = "light_blue_box"
+    case darkBlueBox = "dark_blue_box"
+    case blueBox = "blue_box"
+    case whiteBox = "white_box"
+    case greenBox = "green_box"
+    case redBox = "red_box"
+    case beigeBox = "beige_box"
 }
