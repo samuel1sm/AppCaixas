5dcb8952e893dfbb6de55a063dee6cbde5f7bfed
Samuel Martins <francisco.martins@modal.com.br>
2022-03-03 08:33:24 -0300

    creating registerViewController

---
+++ AppCaixas/view/controllers/Register/RegisterViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RegisterViewController.swift
+//  AppCaixas
+//
+//  Created by FRANCISCO SAMUEL DA SILVA MARTINS on 02/03/22.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+    static let identifier = "RegisterViewController"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
